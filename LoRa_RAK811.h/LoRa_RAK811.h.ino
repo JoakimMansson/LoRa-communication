c2ccde5b7c40a84d78a0f0e5dda37a5e55741ac9
Whit3eo <jockel121.121@gmail.com>
2022-10-09 22:06:44 +0200

    Trying some stuff

---
+++ LoRa_RAK811.h/LoRa_RAK811.h.ino
@@ -0,0 +1,7 @@
+class LoRa
+{
+  public:
+
+    String 
+
+};
--- LoRa_sender/LoRa_sender.ino
+++ LoRa_sender/LoRa_sender.ino
@@ -1,32 +1,56 @@
+#include "RAK811.h"
+//Include Serial Library
 #include "SoftwareSerial.h"
-int RESET_PIN = 12;
-#define TXpin 11   // Set the virtual serial port pins
+
+//Set here if you want to use LoRaWAN or P2P Modes
+#define WORK_MODE LoRaP2P   //  LoRaWAN or LoRaP2P
+//This sets the code to ABP
+#define JOIN_MODE ABP
+
+
+//Specify Data Rate
+//For EU & AU This is recommended to be 5 for SF7, For US use 4
+String dataRate = "5";
+
+
+//The Pi Supply shield uses pins 11 & 10 for serial.
+#define TXpin 11 
 #define RXpin 10
+#define RESET_PIN 12
+//The DebugSerial allows the board to respond back via the USB Serial
 #define DebugSerial Serial
+//Setup the serial port for the board
 SoftwareSerial RAKSerial(RXpin,TXpin);    // Declare a virtual serial port
-//First reset the RAK board
-void setup() {
- //Reset the board
 
- DebugSerial.begin(9600);
- digitalWrite(RESET_PIN, LOW);   // turn the pin low to Reset
- delay(400);
- digitalWrite(RESET_PIN, HIGH);    // then high to enable
- DebugSerial.println("Reset RAK");
- delay(400);
 
- //Start communication
- RAKSerial.begin(115200); // Arduino Shield
- RAKSerial.println("at+uart=9600,8,0,0,0");
-  delay(400);
- digitalWrite(RESET_PIN, LOW);   // turn the pin low to Reset
- delay(400);
- digitalWrite(RESET_PIN, HIGH);    // then high to enable
+bool InitLoRaWAN(void);
+RAK811 RAKLoRa(RAKSerial,DebugSerial);
 
- }
+
+void setup() 
+{
+  RAKSerial.begin(9600);
+  DebugSerial.begin(9600);
+  Serial.begin(9600);
+
+  // mode = 1: sets LoRaP2P
+  RAKLoRa.rk_setWorkingMode(1);
+  RAKLoRa.rk_setConfig("lorap2p:transfer_mode", "1");
+  //lorap2p:869525000:7:0:1:5:5
+  RAKLoRa.rk_initP2P("869525000", 7, 0, 1, 5, 5);
+}
 
 void loop() {
+  //The buffer is the data you wish to send in a hex format. Change this variable to be the data you wish to send.
+  char* buffer = "52544B";
 
-  DebugSerial.println("BAUD SHOULD BE SET");
-  delay(10000);
+
+  String data = RAKLoRa.rk_recvData();
+  Serial.print("Data received: ");
+  Serial.println(data);
+
+  
+
 }
+
+
