18a2ae281201ff123cf8467b16770035d231a5c6
Whit3eo <jockel121.121@gmail.com>
2022-11-04 22:47:10 +0100

    Still working on the class
    Still have slightly annoying error with invalid operands...

--- LoRa_RAK811/LoRa_RAK811.ino
+++ LoRa_RAK811/LoRa_RAK811.ino
@@ -1,43 +1,49 @@
 #include <SoftwareSerial.h>
+#include "string.h"
 
 class LoRa_P2P
 {
 
   private:
-    SoftwareSerial RAKSerial;
+    SoftwareSerial *RAKSerial;
 
-  
-  public:
-
-    LoRa(int rxPin, int txPin) : RAKSerial(rxPin, txPin)
-    {}
-  
     // Sets working mode for RAK811 to p2p
     void config_p2p()
     {
-      String command = "at+set_config=lora:work_mode:1";
-      RAKSerial.println(command);
+      char command[30] = "at+set_config=lora:work_mode:1";
+      RAKSerial->write(command);
     }
+  
+  public:
+
+    LoRa_P2P(int rxPin, int txPin)
+    {
+      this->RAKSerial = new SoftwareSerial(rxPin, txPin);
+      RAKSerial->begin(9600);
+      config_p2p();
+
+    }
+    
 
     // Sets frequency, spreadfactirm bandwidth, codingrate, preamble and power
-    bool set_config(String frequency, int SF, int BW, int CR, int PRlen, int PWR)
+    bool set_config(char frequency[], int SF, int BW, int CR, int PRlen, int PWR)
     {
-      String command = "at+set_config=lorap2p:" + frequency + ":" + SF + ":" + BW + ":" + CR + ":" + PRlen + ":" + PWR;
-      String ret = RAKSerial.println(command);
-      cout << ret;
+      char double_dots[1] = ":";
+      char command[23] = "at+set_config=lorap2p:" + frequency + ":" + char(SF) + ":" + char(BW) + ":" + char(CR) + ":" + char(PRlen) + ":" + char(PWR);
+      String ret = RAKSerial->write(command);
       if(ret == "OK")
       {
-        return True;
+        return true;
       }
       else
       {
-        return False;
+        return false;
       }
     }
 
-    bool set_transfer_mode(bool is_receiver))
+    bool set_transfer_mode(bool is_receiver)
     {
-      String command = "at+set_config=lorap2p: transfer_mode:";
+      char command[12] = "at+set_config=lorap2p: transfer_mode:";
 
       if(is_receiver)
       {
@@ -48,15 +54,35 @@ class LoRa_P2P
         command += 2;
       }
       
-      String ret = RAKSerial.println(command);
-      cout << ret;
+      String ret = RAKSerial->write(command);
     }
 
     bool send_data(String data)
     {
-      String command = "at+send=lorap2p:" + data
+      char command[12] = "at+send=lorap2p:" + data;
+      String ret = RAKSerial.write(command);
+
+      if(ret == "OK")
+      {
+        return true;
+      }
+      else
+      {
+        return false;
+      }
 
     }
+
+    void start_recv()
+    {
+      RAKSerial->listen();
+
+      while(RAKSerial->available() > 0)
+      {
+        char inByte = RAKSerial->read();
+        Serial.write(inByte);
+      }      
+    } 
 
 };
 
