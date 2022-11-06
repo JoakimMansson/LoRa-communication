f9b5524bfabe3c5e0e76d62d439590c14ce6915f
Whit3eo <jockel121.121@gmail.com>
2022-11-06 14:51:16 +0100

    d

--- LoRa_sender.ino
+++ LoRa_sender.ino
@@ -1,6 +1,5 @@
-#include "RAK811.h"
-//Include Serial Library
 #include "SoftwareSerial.h"
+#include <lorap2p>
 
 //Set here if you want to use LoRaWAN or P2P Modes
 #define WORK_MODE LoRaP2P   //  LoRaWAN or LoRaP2P
@@ -23,7 +22,6 @@ String dataRate = "5";
 SoftwareSerial RAKSerial(RXpin,TXpin);    // Declare a virtual serial port
 
 
-bool InitLoRaWAN(void);
 RAK811 RAKLoRa(RAKSerial,DebugSerial);
 
 
